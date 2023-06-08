6abb2b5fd908d3342f27c528c664ed8d550d5f1b
Tom Albrighton <thomas.albrighton@four.io>
2023-06-08 20:14:00 +0930

    Upload build dir

--- .github/actions/build/run.sh
+++ .github/actions/build/run.sh
@@ -1,5 +1,4 @@
 sudo apt-get update && sudo apt-get install -y ninja-build gettext cmake unzip curl;
 
 git clone --depth 1 https://github.com/neovim/neovim --branch $TARGET && \
-  cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && \
-  make install;
+  cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
