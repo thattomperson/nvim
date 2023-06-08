35392be5f9ae30c263c326670530f857b520fe19
Tom Albrighton <thomas.albrighton@four.io>
2023-06-08 20:07:48 +0930

    try sudo

--- .github/actions/build/run.sh
+++ .github/actions/build/run.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y ninja-build gettext cmake unzip curl;
+sudo apt-get update && sudo apt-get install -y ninja-build gettext cmake unzip curl;
 
 git clone --depth 1 https://github.com/neovim/neovim --branch $TARGET && \
   cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && \
