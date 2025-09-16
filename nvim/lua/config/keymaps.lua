62fd3970bf73850bfad0799f35b90f74c87c364f
Thomas Albrighton <thomasalbrighton@gmail.com>
2025-09-16 10:26:09 +0930

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,18 @@ local map = vim.keymap.set
 map("n", "<leader>fa", function()
 	Snacks.terminal("cursor-agent", { auto_close = true })
 end, { desc = "Open Cursor" })
+
+if vim.g.neovide then
+  map('n', '<D-s>', ':w<CR>') -- Save
+  map('v', '<D-c>', '"+y') -- Copy
+  map('n', '<D-v>', '"+P') -- Paste normal mode
+  map('v', '<D-v>', '"+P') -- Paste visual mode
+  map('c', '<D-v>', '<C-R>+') -- Paste command mode
+  map('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
