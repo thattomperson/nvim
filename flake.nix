f7f10d9938cc351f6c12a4b9ffb031111406844f
Thomas Albrighton <thomasalbrighton@gmail.com>
2025-09-26 11:25:51 +0930

    Remove nixvim, update flake inputs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   # Add all your dependencies here
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    nixvim.url = "github:nix-community/nixvim";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
   };
