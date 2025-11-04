8188dd71a6a7a36fe838e037d16fa8be7e229124
Thomas Albrighton <thomasalbrighton@gmail.com>
2025-11-04 14:59:32 +1030

    Update test.lua

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -5,7 +5,9 @@ return {
     opts = {
       adapters = {
         ["neotest-phpunit"] = {
-          phpunit_cmd = "phpunit",
+          phpunit_cmd = function()
+            return "phpunit" -- for `dap` strategy then it must return string (table values will cause validation error)
+          end,
           filter_dirs = { "vendor", "plugins", "lib/vendor" },
         },
       }
