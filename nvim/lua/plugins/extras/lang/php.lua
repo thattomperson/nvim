0671ce8df6f11e55b57c15b4458e301b0f3ab677
Thomas Albrighton <thomasalbrighton@gmail.com>
2025-11-10 09:43:56 +1030

    Fix stuff

--- nvim/lua/plugins/extras/lang/php.lua
+++ nvim/lua/plugins/extras/lang/php.lua
@@ -79,7 +79,14 @@ return {
 			formatters_by_ft = {
 				php = { "php_cs_fixer" },
 			},
-			formatters.php_cs_fixer.append_args = { "--config", ".php-cs-fixer.php;.php-cs-fixer.dist.php;.php_cs;.php_cs.dist;$PHPCSFIXER_CONFIG" },
+			formatters = {
+				php_cs_fixer = {
+					append_args = {
+						"--config",
+						".php-cs-fixer.php;.php-cs-fixer.dist.php;.php_cs;.php_cs.dist;$PHPCSFIXER_CONFIG"
+					},
+				},
+			},
 		},
 	},
 }
